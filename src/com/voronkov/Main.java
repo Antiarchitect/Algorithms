6d2935723ca6e8846fe0f130498d50353242a2b0
Andrey Voronkov <andrey.voronkov@medm.com>
2014-09-16 16:24:36 +0400

    Initial commit.

---
+++ src/com/voronkov/Main.java
@@ -0,0 +1,14 @@
+package com.voronkov;
+
+import java.util.HashMap;
+
+public class Main {
+
+    private static final HashMap<String, String> algorithms = new HashMap<String, String>();
+
+    public static void main(String[] args) {
+        algorithms.put("QF", "QuickFindUF");
+
+    }
+
+}
